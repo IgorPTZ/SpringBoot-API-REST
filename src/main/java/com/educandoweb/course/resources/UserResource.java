811893b2d8a57ed15ba37f41219ff406f268e646
Igor <igor.ptz@hotmail.com>
2020-01-13 22:47:05 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,41 @@
  package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 // Application Flow: Application -> Resource Layer (rest controllers) -> Service Layer -> Data Access Layer (data repositories)
+ 
 
+ // Classe da camada de resource
  @RestController
  @RequestMapping(value ="/users")
  public class UserResource {
 	 
+	// A notation autowired realiza a injeção de dependecias automaticamente
+	 @Autowired
+	 private UserService service;
+	 
 	 @GetMapping
-	 public ResponseEntity<User> findAll() {
-		 User user = new User(1L, "Igor", "igor.ptz@hotmail.com", "71987034333", "teste321");
+	 public ResponseEntity<List<User>> findAll() {
+		 List<User> list = service.findAll();
+		 
+		 return ResponseEntity.ok().body(list);
+	 }
+	 
+	 // Indica que a rota podera receber um parametro do tipo id
+	 @GetMapping(value = "/{id}")
+	 public ResponseEntity<User> findById(@PathVariable Long id) {
+		 User user = service.findById(id);
+		 
 		 return ResponseEntity.ok().body(user);
 	 }
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+//Application Flow: Application -> Resource Layer (rest controllers) -> Service Layer -> Data Access Layer (data repositories)
+
+// A notation @Service permite cadastrar uma classe para que seja injetada automaticamente nas dependencias
+@Service
+public class UserService {
+	// A notation autowired realiza a injeção de dependecias automaticamente
+	
+	@Autowired 
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+}
