90021e7493d8865da490b45638e0a68190ffb484
Igor <igor.ptz@hotmail.com>
2020-01-30 21:10:28 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -37,5 +41,16 @@ import com.educandoweb.course.services.UserService;
 		 User user = service.findById(id);
 		 
 		 return ResponseEntity.ok().body(user);
+	 }
+	 
+	 @PostMapping
+	 public ResponseEntity<User> insert(@RequestBody User user) {
+		 user = service.insert(user);
+		 
+		 URI uri = ServletUriComponentsBuilder.
+				   fromCurrentRequest().path("/{id}").
+				   buildAndExpand(user.getId()).toUri();
+		 
+		 return ResponseEntity.created(uri).body(user);
 	 }
 }
